20712b3cd1505085fa63a44d5f3df0026d76f41e
AntonioBho <lafeminaantonio@libero.it>
2019-05-17 15:24:41 +0200

    up

--- Rest_DHA/src/java/event/Event.java
+++ Rest_DHA/src/java/event/Event.java
@@ -5,9 +5,15 @@
  */
 package event;
 
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
+import com.mongodb.client.model.Filters;
 import java.util.List;
 import javax.ws.rs.*;
 import javax.ws.rs.core.*;
+import org.bson.Document;
 
 
 /**
@@ -18,6 +24,9 @@ import javax.ws.rs.core.*;
 @Path("event")
 public class Event {
 
+    
+    public MongoClient mongoClient = new MongoClient(new MongoClientURI("mongodb://dhagroup:glassfish@45.76.47.94/esse3"));
+
     @Context
     private UriInfo context;
     private String type;
@@ -25,6 +34,7 @@ public class Event {
     private String description;
     private List<String> students;
     private String data;
+    private String id; //data+description
     
     /**
      * Creates a new instance of Event
@@ -37,25 +47,42 @@ public class Event {
      * Retrieves representation of an instance of event.Event
      * @return an instance of java.lang.String
      */
-    //Mostra l'evento
+    //Mostra eventi
     @GET
     @Produces("application/json")
     public String getJson() {
+        
+        //Bisogna fare un'iterazione
+//        MongoCollection<Document> collection = mongoClient.getDatabase("esse3").getCollection("events");
+//        MongoCursor<Document> result =  
         String pattern= "{ \"professor\":\"%s\", \"description\":\"%s\"}";
         return String.format(pattern,professor,description);
     }
     
-    //Modifica o Crea l'event
+    //Crea l'event o Modifica
     @PUT
+    @Path ("/{eventID}")
     @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
     //Quello tra () viene usato nella url mentre dopo il tipo nel codice
-    public String update(@QueryParam("description") String de, @QueryParam("type") String t, @QueryParam("data")  String da, @QueryParam("professor") String prof) {
-        description=de;
-        type=t;
-        data=da;
-        professor=prof;
-        String pattern = "{\"status\":\"%s\", \"professor\":\"%s\", \"description\":\"%s\", \"type\":\"%s\", \"data\": \"%s\"}";
-        return String.format(pattern,"OK",professor, description, type, data );   
+    public String update(@PathParam("eventID") String eventID, @QueryParam("description") String de, @QueryParam("type") String t, @QueryParam("data")  String da, @QueryParam("professor") String prof) {
+        if(de == null) return "{\"status\":\"error\", \"description\":\"description is a mandatory field\"}";
+        if(t == null) return "{\"status\":\"error\", \"description\":\"type is a mandatory field\"}";
+        if(da == null) return "{\"status\":\"error\", \"description\":\"data is a mandatory field\"}";
+        if(prof == null) return "{\"status\":\"error\", \"description\":\"professor is a mandatory field\"}";
+        
+        MongoCollection<Document> collection = mongoClient.getDatabase("esse3").getCollection("events");
+        MongoCursor<Document> result = collection.find(Filters.eq("eventID", eventID)).iterator();
+        if(result.hasNext()) return "{\"status\":\"error\", \"description\":\"Event ID  already exists\"}";
+
+        Document document = new Document()
+                .append("eventID", eventID)
+                .append("type", t)
+                .append("data", da)
+                .append("description", de)
+                .append("professor", prof);
+
+        collection.insertOne(document);
+        return "{\"status\":\"ok\"}";  
 }
 
     //Restituisce i partecipanti

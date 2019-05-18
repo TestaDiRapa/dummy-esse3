0a195b9325f16b6d9e06ab27c6c3b4b392cebcc1
AntonioBho <lafeminaantonio@libero.it>
2019-05-18 13:15:39 +0200

    up

--- Rest_DHA/src/java/event/ApplicationConfig.java
+++ Rest_DHA/src/java/event/ApplicationConfig.java
@@ -31,6 +31,7 @@ public class ApplicationConfig extends Application {
     private void addRestResourceClasses(Set<Class<?>> resources) {
         resources.add(event.Event.class);
         resources.add(professors.Professor.class);
+        resources.add(students.Student.class);
     }
     
 }
